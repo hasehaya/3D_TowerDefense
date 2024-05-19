94d0b19c6a94c44bacfe74667fa59ac155f0a7c5
hasehaya <runcona8@gmail.com>
2024-05-19 18:09:21 +0900

    フォルダ整理：Enemy整理

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,19 +2,29 @@
 using UnityEngine.AI;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class Enemy :MonoBehaviour
 {
     // 破壊された際に呼ばれるイベント
     public delegate void EnemyDestroyed(Enemy enemy);
     public static event EnemyDestroyed OnEnemyDestroyed;
-
+    // ナビゲーション
     NavMeshAgent nav;
     // HP関係
     Slider slider;
     [SerializeField] GameObject hpBar;
+    // 敵の種類
+    public enum EnemyType
+    {
+        None = 0,
+        Slime = 1,
+        Turtle = 2,
+    }
+    [SerializeField] EnemyType enemyType;
     // ステータス
+    GameObject enemyPrefab;
     float hp = 10;
-    float speed = 1.0f;
+    float speed = 2f;
     float attackPower = 1.0f;
     float attackSpeed = 1.0f;
     float attackRange = 1.0f;
@@ -22,14 +32,29 @@ public class Enemy :MonoBehaviour
 
     void Start()
     {
+        SetStatus();
         SetNavigation();
         SetHpSlider();
+        AddRigidBody();
+    }
+
+    void SetStatus()
+    {
+        var status = EnemyManager.Instance.GetEnemyStatus(enemyType);
+        enemyPrefab = status.enemyPrefab;
+        hp = status.hp;
+        speed = status.speed;
+        attackPower = status.attackPower;
+        attackSpeed = status.attackSpeed;
+        attackRange = status.attackRange;
+        attribute = status.attribute;
     }
 
     void SetNavigation()
     {
         nav = GetComponent<NavMeshAgent>();
         SetDestination(GameManager.Instance.GetBase().transform);
+        nav.speed = speed;
     }
 
     void SetHpSlider()
@@ -42,6 +67,11 @@ public class Enemy :MonoBehaviour
         slider = sliderObj.GetComponentInChildren<Slider>();
         slider.maxValue = hp;
         slider.value = hp;
+    }
+
+    void AddRigidBody()
+    {
+        gameObject.AddComponent<Rigidbody>();
     }
 
     public void SetDestination(Transform destination)
@@ -58,5 +88,31 @@ public class Enemy :MonoBehaviour
             OnEnemyDestroyed?.Invoke(this);
             Destroy(gameObject);
         }
+    }
+}
+
+
+[System.Serializable]
+public class EnemyStatus
+{
+    public Enemy.EnemyType enemyType;
+    public GameObject enemyPrefab;
+    public float hp;
+    public float speed;
+    public float attackPower;
+    public float attackSpeed;
+    public float attackRange;
+    public Attribute attribute;
+
+    public EnemyStatus()
+    {
+        enemyType = Enemy.EnemyType.None;
+        enemyPrefab = null;
+        hp = 0;
+        speed = 0;
+        attackPower = 0;
+        attackSpeed = 0;
+        attackRange = 0;
+        attribute = Attribute.None;
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class EnemyManager :MonoBehaviour
+{
+    private static EnemyManager instance;
+    public static EnemyManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<EnemyManager>();
+            }
+            return instance;
+        }
+    }
+    [SerializeField] EnemyStatusListEntity enemyStatusListEntity;
+
+    public EnemyStatus GetEnemyStatus(Enemy.EnemyType enemyType)
+    {
+        foreach (var enemyStatus in enemyStatusListEntity.lists)
+        {
+            if (enemyStatus.enemyType == enemyType)
+            {
+                return enemyStatus;
+            }
+        }
+        return null;
+    }
+}
