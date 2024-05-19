e516ef8ef6be1d9ae16347d70f0a325364bd93f6
hasehaya <runcona8@gmail.com>
2024-05-19 18:09:40 +0900

    追加：属性追加

--- Assets/Scripts/Attribute.cs
+++ Assets/Scripts/Attribute.cs
@@ -1,5 +1,12 @@
-﻿public enum Attribute
+﻿[System.Serializable]
+public enum Attribute
 {
     None = 0,
     Fire = 1,
+    Water = 2,
+    Wind = 3,
+    Thunder = 4,
+    Ice = 5,
+    Wood = 6,
+    Stone = 7,
 }
