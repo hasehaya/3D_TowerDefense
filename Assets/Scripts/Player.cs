b6b2e782ce4708f4e478ab68d705ec6f9e97c272
RUNCONA8\runco <runcona8@gmail.com>
2024-09-23 22:30:56 +0900

    Player動き完成

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections; // コルーチンを使用するために必要
+﻿using System.Collections;
 
 using UnityEngine;
 
@@ -16,61 +16,159 @@ public class Player :MonoBehaviour
             return instance;
         }
     }
+
     [SerializeField] Transform cameraTransform;
+    [SerializeField] private float moveSpeed = 5f;       // 移動速度
+    [SerializeField] private float rotationSpeed = 720f; // 回転速度
+    [SerializeField] private float jumpForce = 5f;       // ジャンプ力
+
     Rigidbody rb;
     Animator animator;
-    Quaternion targetRotation = Quaternion.identity;
+
+    private bool isGrounded = true; // 現在の地面との接地状態
+    private bool isJumping = false; // ジャンプ中かどうかのフラグ
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        rb.velocity = Vector3.zero;
-        targetRotation = transform.rotation;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // ジャンプ入力
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isJumping)
         {
-            StartCoroutine(Jump());
+            Jump();
         }
-    }
 
-    IEnumerator Jump()
-    {
-        animator.SetBool("isJump", true);
-        yield return new WaitForSeconds(2);
-        animator.SetBool("isJump", false);
+        // アニメーションの更新
+        UpdateAnimator();
     }
 
     void FixedUpdate()
     {
         Move();
+        Rotate();
+        GroundCheck();
+        Gravity();
     }
 
     void Move()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var horizontalRotation = Quaternion.AngleAxis(cameraTransform.eulerAngles.y, Vector3.up);
-        var velocity = horizontalRotation * new Vector3(horizontal, 0, vertical).normalized;
+        // 入力取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        var speed = Input.GetKey(KeyCode.LeftShift) ? 1 : 0.5f;
-        var rotationSpeed = 600 * Time.deltaTime;
+        // カメラの向きに合わせた移動方向
+        Vector3 inputDirection = new Vector3(horizontal, 0, vertical);
 
-        if (velocity.magnitude > 0.5f)
+        // 入力がある場合のみ処理を行う
+        if (inputDirection.sqrMagnitude > 0.01f)
         {
-            targetRotation = Quaternion.LookRotation(velocity, Vector3.up);
+            inputDirection = inputDirection.normalized;
+
+            Quaternion cameraRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+            Vector3 moveDirection = cameraRotation * inputDirection;
+
+            // 移動速度の計算
+            float speedModifier = Input.GetKey(KeyCode.LeftShift) ? 1f : 0.5f;
+            Vector3 velocity = moveDirection * moveSpeed * speedModifier;
+
+            // Rigidbodyに速度を適用（垂直方向の速度は維持）
+            rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
         }
+        else
+        {
+            // 入力がない場合、水平方向の速度をゼロにする
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
 
-        animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
+    void Rotate()
+    {
+        // 入力取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+        Vector3 inputDirection = new Vector3(horizontal, 0, vertical);
+
+        // 入力がある場合のみ回転処理を行う
+        if (inputDirection.sqrMagnitude > 0.01f)
+        {
+            inputDirection = inputDirection.normalized;
+
+            // カメラの向きに合わせた移動方向
+            Quaternion cameraRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+            Vector3 moveDirection = cameraRotation * inputDirection;
+
+            // ターゲットの回転方向
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+            // スムーズに回転
+            Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            rb.MoveRotation(newRotation);
+        }
+        else
+        {
+            // 入力がない場合、回転速度をゼロにする
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        animator.SetBool("isJump", true);
+    }
+
+    void GroundCheck()
+    {
+        // プレイヤーの足元にレイを飛ばして地面をチェック
+        RaycastHit hit;
+        int layerToTarget = LayerMask.NameToLayer("Ground"); // レイヤー名を使用
+        LayerMask layerMask = 1 << layerToTarget;
+        var position = transform.position + Vector3.up * 2;
+
+        if (Physics.Raycast(position, Vector3.down, out hit, 2.01f, layerMask))
+        {
+            if (isJumping)
+            {
+                isGrounded = true;
+                isJumping = false;
+
+                animator.SetBool("isJump", false);
+            }
+        }
+        else
+        {
+            isGrounded = false;
+            isJumping = true;
+        }
+    }
+
+    void Gravity()
+    {
+        rb.AddForce(Vector3.down * 20, ForceMode.Acceleration);
+    }
+
+    void UpdateAnimator()
+    {
+        // 入力取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // スピードの計算
+        float speed = new Vector2(horizontal, vertical).magnitude;
+        float speedModifier = Input.GetKey(KeyCode.LeftShift) ? 1f : 0.5f;
+
+        // アニメーションの速度を設定
+        animator.SetFloat("Speed", speed * speedModifier, 0.1f, Time.deltaTime);
     }
 
     public void WarpTo(Vector3 position)
     {
         transform.position = position;
+        rb.velocity = Vector3.zero; // 速度をリセット
     }
 }
