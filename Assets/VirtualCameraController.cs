f005e6e9a509b97f167bd32facc4256942b1c663
RUNCONA8\runco <runcona8@gmail.com>
2024-11-11 20:08:16 +0900

    Pause中カメラが動かないように

---
+++ Assets/VirtualCameraController.cs
@@ -0,0 +1,34 @@
+﻿using Cinemachine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class VirtualCameraController :MonoBehaviour
+{
+    CinemachineVirtualCamera virtualCamera;
+
+    private void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        StageManager.OnPause += Pause;
+        StageManager.OnResume += Resume;
+    }
+
+    private void OnDestroy()
+    {
+        StageManager.OnPause -= Pause;
+        StageManager.OnResume -= Resume;
+    }
+
+    private void Pause()
+    {
+        virtualCamera.enabled = false;
+    }
+
+    private void Resume()
+    {
+        virtualCamera.enabled = true;
+    }
+}
